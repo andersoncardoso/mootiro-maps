8fa145b044961ffb5d00fc913944c18100825896
Luiz Armesto <luiz.armesto@gmail.com>
2013-01-17 03:42:43 -0200

    building

--- mootiro_komoo/static/tests/test-features.js
+++ mootiro_komoo/static/tests/test-features.js
@@ -1,37 +1,40 @@
+(function() {
 
-require(['map/features'], function(features) {
-  var _this = this;
-  module('Map Features');
-  test('Basic requirements', function() {
-    expect(3);
-    ok(Function.call, 'Function.call()');
-    ok(Object.create, 'Object.create()');
-    return ok(google.maps, 'Google maps API');
+  require(['map/features'], function(features) {
+    var _this = this;
+    module('Map Features');
+    test('Basic requirements', function() {
+      expect(3);
+      ok(Function.call, 'Function.call()');
+      ok(Object.create, 'Object.create()');
+      return ok(google.maps, 'Google maps API');
+    });
+    return test('Get geoJson geometry from feature', function() {
+      var coordinates, feature, geometry, point;
+      expect(1);
+      coordinates = [1, 2];
+      point = {
+        getGeometryType: function() {
+          return 'Point';
+        },
+        getCoordinates: function() {
+          return coordinates;
+        },
+        getGeoJson: function() {
+          return {
+            type: this.getGeometryType(),
+            coordinates: this.getCoordinates()
+          };
+        }
+      };
+      feature = new features.Feature();
+      geometry = {
+        'type': 'Point',
+        'coordinates': coordinates
+      };
+      feature.setGeometry(point);
+      return deepEqual(feature.getGeoJsonGeometry(), geometry, 'Should return the geometry containing the coordinates it received');
+    });
   });
-  return test('Get geoJson geometry from feature', function() {
-    var coordinates, feature, geometry, point;
-    expect(1);
-    coordinates = [1, 2];
-    point = {
-      getGeometryType: function() {
-        return 'Point';
-      },
-      getCoordinates: function() {
-        return coordinates;
-      },
-      getGeoJson: function() {
-        return {
-          type: this.getGeometryType(),
-          coordinates: this.getCoordinates()
-        };
-      }
-    };
-    feature = new features.Feature();
-    geometry = {
-      'type': 'Point',
-      'coordinates': coordinates
-    };
-    feature.setGeometry(point);
-    return deepEqual(feature.getGeoJsonGeometry(), geometry, 'Should return the geometry containing the coordinates it received');
-  });
-});
+
+}).call(this);
