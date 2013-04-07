421a7347a7fcdfb828a1f42ecdc2257f658bddf6
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2013-04-07 19:01:54 -0300

    adding api to need + general fns for apis (DRY)

--- mootiro_komoo/apps/komoo_resource/api.py
+++ mootiro_komoo/apps/komoo_resource/api.py
@@ -3,9 +3,7 @@ from __future__ import unicode_literals
 import logging
 
 from main.utils import APIHandler
-from main.utils import JsonResponse
-from main.utils import get_json_data
-# from main.datalog import log_data
+from main.api import api_add_new, api_get_by_id, api_update_by_id
 from .models import Resource_CO as Resource
 
 
@@ -16,55 +14,15 @@ class ResourcesHandler(APIHandler):
     """ /resources """
 
     def post(self, request):
-        """
-        Responsible for creating a new Resource
-        """
-        json_data = get_json_data(request)
-        resource = Resource()
-        resource.from_dict(json_data)
-
-        if not request.user.can_edit(resource):
-            return JsonResponse(
-                    request.user.no_permission_message, response_type='error')
-
-        resource.creator = request.user
-        if not resource.is_valid():
-            return JsonResponse(resource.errors, response_type='error')
-
-        resource.save()
-        return JsonResponse({'redirect': resource.url})
+        return api_add_new(Resource, request)
 
 
 class ResourcesIDHandler(APIHandler):
     """ /resources/[id_] """
 
     def get(self, request, id_):
-        resource = Resource.get_by_id(id_)
-
-        if not resource:
-            return JsonResponse(response_type='not_found')
-
-        # resource_data = resource.to_cleaned_dict(user=request.user)
-        resource_data = resource.to_dict()
-        return JsonResponse(resource_data)
+        return api_get_by_id(Resource, request, id_)
 
     def put(self, request, id_):
-        """ Updates user data """
-        json_data = get_json_data(request)
-        res = Resource.get_by_id(id_)
-        if not res:
-            JsonResponse(response_type='not_found')
-
-        if not request.user.can_edit(res):
-            return JsonResponse(
-                    request.user.no_permission_message, response_type='error')
-
-        res.from_dict(json_data)
-        if not res.is_valid():
-            return JsonResponse(res.errors, response_type='error')
-
-        res.save()
-        # log_data.send(
-        #         sender=res, object_=res, user=request.user, action='E')
-        return JsonResponse({'redirect': res.url})
+        return api_update_by_id(Resource, request, id_)
 
---
+++ mootiro_komoo/apps/main/api.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import logging
+
+from main.utils import JsonResponse
+from main.utils import get_json_data
+# from main.datalog import log_data
+
+
+logger = logging.getLogger(__name__)
+
+
+def api_add_new(model, request):
+    """ Responsible for creating a new `model` object """
+    json_data = get_json_data(request)
+    obj = model()
+    obj.from_dict(json_data)
+
+    if not request.user.can_edit(obj):
+        return JsonResponse(
+                request.user.no_permission_message, response_type='error')
+
+    obj.creator = request.user
+    if not obj.is_valid():
+        return JsonResponse(obj.errors, response_type='error')
+
+    obj.save()
+    return JsonResponse({'redirect': obj.url})
+
+
+def api_get_by_id(model, request, id_):
+    obj = model.get_by_id(id_)
+
+    if not obj:
+        return JsonResponse(response_type='not_found')
+
+    # obj_data = obj.to_cleaned_dict(user=request.user)
+    obj_data = obj.to_dict()
+    return JsonResponse(obj_data)
+
+
+def api_update_by_id(model, request, id_):
+    """ Updates user data """
+    json_data = get_json_data(request)
+    obj = model.get_by_id(id_)
+    if not obj:
+        JsonResponse(response_type='not_found')
+
+    if not request.user.can_edit(obj):
+        return JsonResponse(
+                request.user.no_permission_message, response_type='error')
+
+    obj.from_dict(json_data)
+    if not obj.is_valid():
+        return JsonResponse(obj.errors, response_type='error')
+
+    obj.save()
+    # log_data.send(
+    #         sender=res, object_=obj, user=request.user, action='E')
+    return JsonResponse({'redirect': obj.url})
+
